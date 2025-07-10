50c6648fe3c4d7646847a3c24b635a5c1d1cccb0
Dysthesis <antheoraviel@protonmail.com>
2025-07-11 08:53:17 +1000

    Set up tree sitter

--- nix/packages/foucault.nix
+++ nix/packages/foucault.nix
@@ -1,5 +1,7 @@
 {
+  tree-sitter-grammars,
   kak-lsp,
+  kak-tree-sitter,
   pkgs,
   self,
   ripgrep,
@@ -18,20 +20,60 @@
     text = builtins.readFile "${self}/kakrc.kak";
   };
 
+  tree-sitter-conf = writeTextFile rec {
+    name = "config.toml";
+    destination = "/kak-tree-sitter/${name}";
+    text = with tree-sitter-grammars;
+      lib.foldlAttrs
+      (
+        acc: name: vals:
+          acc
+          + ''
+            [language.${name}.grammar.source.local]
+            path = "${vals.parser}"
+            [language.${name}.grammar]
+            compile = "cc"
+            compile_args = ["-c", "-fpic", "../scanner.c", "../parser.c", "-I", ".."]
+            compile_flags = ["-O3"]
+            link = "cc"
+            link_args = ["-shared", "-fpic", "scanner.o", "parser.o", "-o", "typescript.so"]
+            link_flags = ["-O3"]
+            [language.${name}.queries.source.local]
+            path = "${vals.queries}"
+            [language.${name}.queries]
+            path = "${vals.queries}"
+          ''
+      )
+      ""
+      {
+        nix = {
+          parser = tree-sitter-nix + "/parser";
+          queries = tree-sitter-nix + "/queries";
+        };
+        rust = {
+          parser = tree-sitter-rust + "/parser";
+          queries = tree-sitter-rust + "/queries";
+        };
+      };
+  };
+
   customKakoune = kakoune.override {
     plugins = with kakounePlugins; [
       config
       parinfer-rust
       smarttab-kak
+      kakoune-catppuccin
     ];
   };
   deps = [
     kak-lsp
+    kak-tree-sitter
     ripgrep
     fd
   ];
 in
   mkWrapper pkgs customKakoune ''
     wrapProgram $out/bin/kak \
-      --prefix PATH ":" ${makeBinPath deps}
+      --prefix PATH ":" ${makeBinPath deps} \
+      --set XDG_CONFIG_HOME "${tree-sitter-conf}"
   ''
